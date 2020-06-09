d09be8bac20ef413463f3720bf8478ae12063cb1
Anatoly Rodin <anatoly.richman@gmail.com>
2020-06-10 01:19:38 +0300

    Created footer component.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component'
 import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
