7d9fc20ef6edecfa9fb2ae05af91db4bb0782ff3
Anatoly Rodin <anatoly.richman@gmail.com>
2020-06-10 01:23:04 +0300

    Created component for 404 page.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common'
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
+import { NotFound404Component } from './components/not-found404/not-found404.component'
 
 @NgModule({
   declarations: [
     MainLayoutComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFound404Component
   ],
   imports: [
     CommonModule
